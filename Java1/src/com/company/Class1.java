ec4521c33fbb26aef89f635f18cac2e123de33ea
Caitlyn Chen <caitlynachen@gmail.com>
2020-04-20 09:34:44 -0700

    python class 1

--- Java1/src/com/company/Class1.java
+++ Java1/src/com/company/Class1.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.Scanner;
 
-public class Main {
+public class Class1 {
 
     public static void main(String[] args) {
 	// write your code here
---
+++ Python1/class1.py
@@ -0,0 +1,7 @@
+a = 1;
+b = 2;
+zero = 0;
+s = a + b;
+
+print(a)
+print(s)
