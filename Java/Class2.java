dc887e1f65db4b6c852c1471e4fe2600ca59fe98
Shivaum <shivaum1@gmail.com>
2020-04-26 18:30:28 -0700

    week 2

--- Java/Class2.java
+++ Java/Class2.java
@@ -1,62 +1,44 @@
 import java.util.Scanner;
-import java.lang.Math;
 
-public class Class1 {
+public class Class2 {
 
     public static void main(String[] args) {
-	// write your code here
-        // STRONGLY TYPED - COMPILED LANGUAGE
-        System.out.println("Hello World!");
-        // The value stored in a variable can be changed during program execution.
-        //A variable is only a name given to a memory location, all the operations done on the variable effects that memory location.
-        //In Java, all the variables must be declared before use.
-        int a = 1;
-        int b = 2;
-        int zero = 0;
-        int sum = a + b;
-        /*
-        Compile Time Errors:
-        cannot change types to variable
-         Incompatible types
-        a = 3.0;
-         Syntax Error
-        A = 3;
-        */
-        //int rounds down
-        System.out.println(a / b);
-        System.out.println(a % b);
-        a = 2;
-        System.out.println(Math.pow(a, b));
         /*
         Runtime Error:
          Zero division error
         int zero_division = a / zero;
         System.out.println(zero_division);
         */
+        // go over camel casing
+        // quick hw review
+        // review from concepts from last class
+        int myAge = 19;
+        int yourAge = 25;
 
-        //System.out.println(sum);
-
-        double c = 1.5;
-        double d = 2.4;
-        double dSum = c + d;
-        d = 1;
-        d++;
+        int d = 5;
+        int c = 3;
         d *= c;
 
-        System.out.println(dSum);
+        System.out.println(d);
+
+        a = 10;
+        b = 3;
+        System.out.println(a % b);
 
         boolean isHappy = true;
 
+        // can be useful when looking at strings
         char e = 'c';
 
         System.out.println("This apple costs " + a + " dollars");
 
         String str = "This is a string.";
+        // explain index in java (0 is first letter)
         String new_str = str.substring(10, 16);
         System.out.println(new_str);
 
-        String another_str = "Another string";
-        str += another_str;
+        String anotherStr = "Another string";
+        str += anotherStr;
 
         // indexes start from 0
         //returns index of the first char/string
