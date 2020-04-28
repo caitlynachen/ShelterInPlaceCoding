d01050a8ed4c8a066c45a4f37643a5546d6c9039
Caitlyn Chen <caitlynachen@gmail.com>
2020-04-27 17:36:40 -0700

    hw1 java

---
+++ Java/HW1.java
@@ -0,0 +1,8 @@
+public class HW1 {
+	public static void main(String[] args) {
+		System.out.println("Hello! My name is Shivaum Kumar and I am 19 years old.");
+		System.out.println("I have been programming for about 4 years. I love computer science!");
+		System.out.println("I have about 1.5 years left of college. I will be graduating in 2022.");
+
+	}
+}
